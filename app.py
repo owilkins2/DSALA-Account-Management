14b544ec4bc6d5d500de74a430a18c864a3291af
Oliver Wilkins <wilkins@oxy.edu>
2024-09-30 10:59:24 -0700

    editing functionality

--- app.py
+++ app.py
@@ -32,12 +32,32 @@ def create_client():
 
 @app.route('/edit-client-enter-id')
 def edit_client_enter_id():
-    return render_template('edit_client_enter_id.html')
+    return render_template('edit_client_enter_id.html', )
 
-@app.route('/entered_id')
+@app.route('/entered_id', methods=['POST'])
 def entered_id():
-    print("hello")
-    return render_template('edit_client.html')
+    ID = request.form.get('ID')
+    first_name = ''
+    last_name = ''
+    dob = ''
+    email = ''
+    
+    existing_users = DS_Clients.get_all_records()
+
+    print(ID)
+    for user in existing_users:
+        print(user['ID'])
+    user = next((user for user in existing_users if (int(user['ID']) - int(ID) == 0)), None)
+    if user:
+        print(user.keys())
+        first_name = user['First Name']
+        last_name = user['Last Name']
+        dob = user['DOB']
+        email = user['Email']
+        return render_template('edit_client.html', ID=ID, first_name=first_name, last_name=last_name, dob=dob, email=email)
+        
+    else:
+        return "User not found. Try again."
 
 def generate_unique_id(existing_ids):
     while True:
@@ -71,24 +91,31 @@ def submit():
 
 @app.route('/edit', methods=['POST'])
 def edit():
-    email = request.form['email']
-    new_name = request.form['name']
-    
-    existing_users = DS_Clients.get_all_records()
-    for i, user in enumerate(existing_users):
-        if user['Email'] == email:
-            DS_Clients.update_cell(i+2, 1, new_name)
-            return redirect('/success')
+        ID = request.form['ID']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        dob = request.form['dob']
 
-    return redirect('/notfound')
+        existing_users = DS_Clients.get_all_records()
+        row_to_update = None
+        for index, user in enumerate(existing_users):
+            if int(user['ID']) == int(ID):
+                row_to_update = index + 2
+                break
+
+        if row_to_update:
+            new_values = [ID, first_name, last_name, email, dob]
+       
+            DS_Clients.update(f'A{row_to_update}:E{row_to_update}', [new_values])
+            return "User updated successfully."
+        else:
+            return "User not found."
 
 @app.route('/success')
 def success():
     return "Your information has been saved!"
 
-@app.route('/notfound')
-def notfound():
-    return "No user found with that email!"
 
 if __name__ == "__main__":
     app.run(debug=True)
