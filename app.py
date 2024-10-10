ecac6b977c38ecf44e2e5f365bd2ffb3950041d3
Oliver Wilkins <wilkins@oxy.edu>
2024-10-10 16:25:45 -0700

    updated website to rely on csv files for fields to include for easier editing

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import random
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 from dotenv import load_dotenv
+import csv
 
 load_dotenv()
 
@@ -22,6 +23,22 @@ client = gspread.authorize(creds)
 DS_Clients = client.open("DSALA Client Database").sheet1
 Contacts_Sheet = client.open("DSALA Client Database").get_worksheet(1)
 
+def get_client_fields():
+    client_fields = []
+    with open('client_fields.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            client_fields.append(row)
+    return client_fields
+
+def get_contact_fields():
+    contact_fields = []
+    with open('contact_fields.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            contact_fields.append(row)
+    return contact_fields
+
 def get_contacts(ID):
     print("hello")
     all_contacts = Contacts_Sheet.get_all_records()
@@ -35,33 +52,28 @@ def get_contacts(ID):
     return contacts
 
 def get_client_info(ID):
-    first_name = ''
-    last_name = ''
-    dob = ''
-    email = ''
+    fields = get_client_fields()
+    client_info = []
     
     existing_users = DS_Clients.get_all_records()
 
     user = next((user for user in existing_users if (int(user['ID']) - int(ID) == 0)), None)
     if user:
-        print(user.keys())
-        first_name = user['First Name']
-        last_name = user['Last Name']
-        dob = user['DOB']
-        email = user['Email']
-        return([first_name, last_name, email, dob])
+        for field in fields:
+            client_info.append(user[field[1]])
+        return(client_info)
     else:
         return 'error'
 
 def get_contact_info(ID, index):
+    fields = get_contact_fields()
     all_contacts = Contacts_Sheet.get_all_records()
     contact_info = []
+
     for contact in all_contacts:
         if int(contact["DS Client's ID"]) - int(ID) == 0 and int(contact["Index"]) - int(index) == 0:
-            contact_info.append(contact['First Name'])
-            contact_info.append(contact['Last Name'])
-            contact_info.append(contact['Email'])
-            contact_info.append(contact['Relationship'])
+            for field in fields:
+                contact_info.append(contact[field[1]])
     return contact_info
     
 
@@ -75,27 +87,26 @@ def find_client():
 
 @app.route('/create-client')
 def create_client():
-    return render_template('create_client.html')
+    return render_template('create_client.html', client_fields=get_client_fields())
 
 @app.route('/create-contact')
 def create_connection():
     ID = request.args.get('ID')
     first_name = request.args.get('first_name')
     last_name = request.args.get('last_name')
-    return render_template('create_contact.html', ID=ID, first_name=first_name, last_name=last_name)
+    return render_template('create_contact.html', ID=ID, first_name=first_name, last_name=last_name, contact_fields=get_contact_fields())
 
 
 @app.route('/test')
 def test():
-    test_list = ["test 1", "test 2", "test 3"]
-    return render_template('test.html', list=test_list)
+    return render_template('test.html', client_fields=get_client_fields())
 
 
 @app.route('/entered_id', methods=['POST'])
 def entered_id():
     ID = request.form.get('ID')
     
-    return render_template('edit_client.html', ID=ID, info=get_client_info(ID), contacts=get_contacts(ID))
+    return render_template('edit_client.html', ID=ID, info=get_client_info(ID), contacts=get_contacts(ID), client_fields=get_client_fields())
         
 
 def generate_unique_id(existing_ids):
@@ -112,39 +123,39 @@ def edit_contact_button():
     index = request.args.get('index')
     info = get_contact_info(ID, index)
 
-    return render_template('edit_contact.html', ID=ID, index=index, info=get_contact_info(ID, index))
+    return render_template('edit_contact.html', ID=ID, index=index, info=get_contact_info(ID, index), contact_fields=get_contact_fields())
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    email = request.form['email']
-    dob = request.form['dob']
+    fields = get_client_fields()
+    information = []
 
     # Check if the user already exists
     existing_users = DS_Clients.get_all_records()
     existing_ids = [user['ID'] for user in existing_users if 'ID' in user]
 
-    for i, user in enumerate(existing_users):
-        if user['Email'] == email:
-            return "this email is already associated with an existing client"
+#    for i, user in enumerate(existing_users):
+#        if user['Email'] == email:
+#            return "this email is already associated with an existing client"
 
     # If user doesn't exist, generate a unique random ID
     ID = generate_unique_id(existing_ids)
+    information.append(ID)
+    for field in fields:
+        information.append(request.form[field[0]])
 
-    # Append new user with the unique ID
-    DS_Clients.append_row([ID, first_name, last_name, email, dob])
-    return render_template('edit_client.html', ID=ID, info=get_client_info(ID), contacts=get_contacts(ID))
+    DS_Clients.append_row(information)
+    return render_template('edit_client.html', ID=ID, info=get_client_info(ID), contacts=get_contacts(ID), client_fields=get_client_fields())
 
 
 @app.route('/submit-contact', methods=['POST'])
 def submit_connection():
+    fields = get_contact_fields()
+    information = []
+
     ID = request.form['ID']
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    email = request.form['email']
-    relationship = request.form['relationship']
+    information.append(ID)
 
     existing_contacts = get_contacts(ID)
     indexes = []
@@ -155,16 +166,27 @@ def submit_connection():
     if len(indexes) > 0:
         new_index = max(indexes) + 1
 
-    Contacts_Sheet.append_row([ID, new_index, first_name, last_name, email, relationship])
-    return render_template('edit_client.html', ID=ID, info=get_client_info(ID), contacts=get_contacts(ID))
+    information.append(new_index)
+
+    for field in fields:
+        information.append(request.form[field[0]])
+
+    Contacts_Sheet.append_row(information)
+    return render_template('edit_client.html', ID=ID, info=get_client_info(ID), contacts=get_contacts(ID), client_fields=get_client_fields())
+
 
 @app.route('/edit', methods=['POST'])
 def edit():
+        fields = get_client_fields()
+        information = []
+
         ID = request.form['ID']
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        email = request.form['email']
-        dob = request.form['dob']
+        information.append(ID)
+
+        print(request.form)  # This will show you all the submitted form data
+        for field in fields:
+            print(field[0])
+            information.append(request.form[field[0]])
 
         existing_users = DS_Clients.get_all_records()
         row_to_update = None
@@ -174,21 +196,27 @@ def edit():
                 break
 
         if row_to_update:
-            new_values = [ID, first_name, last_name, email, dob]
+            start_column = 'A'
+            end_column = chr(ord(start_column) + len(information) - 1)
        
-            DS_Clients.update(f'A{row_to_update}:E{row_to_update}', [new_values])
-            return "User updated successfully."
+            DS_Clients.update(f'{start_column}{row_to_update}:{end_column}{row_to_update}', [information])
+            return render_template('edit_client.html', ID=ID, info=get_client_info(ID), contacts=get_contacts(ID), client_fields=get_client_fields())
         else:
             return "User not found."
 
+
 @app.route('/edit-contact', methods=['POST'])
 def edit_contact():
+        fields = get_contact_fields()
+        information = []
+
         ID = request.form['ID']
         index = request.form['index']
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        email = request.form['email']
-        relationship = request.form['relationship']
+        information.append(ID)
+        information.append(index)
+
+        for field in fields:
+            information.append(request.form[field[0]])
 
         existing_contacts = Contacts_Sheet.get_all_records()
         row_to_update = None
@@ -198,10 +226,11 @@ def edit_contact():
                 break
 
         if row_to_update:
-            new_values = [ID, index, first_name, last_name, email, relationship]
+            start_column = 'A'
+            end_column = chr(ord(start_column) + len(information) - 1)
        
-            Contacts_Sheet.update(f'A{row_to_update}:F{row_to_update}', [new_values])
-            return render_template('edit_client.html', ID=ID, info=get_client_info(ID), contacts=get_contacts(ID))
+            Contacts_Sheet.update(f'{start_column}{row_to_update}:{end_column}{row_to_update}', [information])
+            return render_template('edit_client.html', ID=ID, info=get_client_info(ID), contacts=get_contacts(ID), client_fields=get_client_fields())
         else:
             return "User not found."
 
